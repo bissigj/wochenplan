caffde5e54e8c3c6c2ec13fe28755daf26ec64e0
bissigj <jonas_bissig@hotmail.com>
2026-04-21 15:40:30 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,13 @@
+export const state = {
+  recipes: [],
+  weekPlan: { kw: '', year: 0, days: [], portions: 2 },
+  archive: [],
+  nextId: 1,
+  settings: { cats: [], aufwand: [], einheiten: [] },
+  familyId: null,
+  familyName: '',
+  userId: null,
+  userEmail: '',
+  activeTab: 'rezepte',
+  recipeFilter: ''
+};
